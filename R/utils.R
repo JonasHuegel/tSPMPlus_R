92e97427aab6e558d2a187f2b3a4cd6d58bf6956
Jonas Hügel <huegel.jonas@gmail.com>
2023-04-24 21:06:21 -0400

    added util function to convert alpha-numeric dbmarts into numeric ones \n addded documentation files for functions

---
+++ R/utils.R
@@ -0,0 +1,41 @@
+transformDbMartToNumeric<- function(dbmart){
+  require(dplyr)
+  require(tidyr)
+  require(DT)
+  require(data.table)
+  patient_num <- c(unique(dbmart$patient_num))
+  
+  patLookUp <- as.data.frame(patient_num)
+  
+  setDT(patLookUp)
+  
+  patLookUp[,num_pat_num := .I]
+  patLookUp <- patLookUp %>% mutate(num_pat_num = num_pat_num -1)
+  
+  dbmart_num <- dbmart %>% dplyr::left_join(patLookUp, by="patient_num")
+  
+  phenx <- c(unique(dbmart$phenx))
+  
+  phenxLookUp <- as.data.frame(phenx)
+  
+  setDT(phenxLookUp)
+  
+  phenxLookUp[,num_Phenx := .I]
+  
+  dbmart_num <- dbmart_num %>% dplyr::left_join(phenxLookUp, by="phenx")
+  
+  dbmart_num <- dbmart_num %>% select(num_pat_num, num_Phenx, start_date)
+  
+  dbmart_num <- dbmart_num[order(dbmart_num$num_pat_num,dbmart_num$start_date),]
+  
+  rownames(dbmart_num) = seq(length=nrow(dbmart_num))
+  
+  dbmart_num$start_date <- as.Date(dbmart_num$start_date)
+  
+  out <- list()
+  out$patientLookUp <- patLookUp
+  out$phenxLookUp <- phenxLookUp
+  out$dbMart <- dbmart_num
+  
+  return (out)
+}
