13ce3b722252580e1d1eb4b2046eafc04a332bf0
Jonas <huegel.jonas@gmail.com>
2023-05-10 14:42:55 -0400

    added functions for phenx and sequence of intrest extraction

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -84,12 +84,43 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// getSequencesWithCandidateEnd
+DataFrame getSequencesWithCandidateEnd(DataFrame& df_dbMart, std::string outputDir, std::string outputFilePrefix, unsigned long minDuration, unsigned int bitShift, unsigned int lengthOfPhenx, unsigned int numOfBuckets, IntegerVector& lowerBucketThresholds, IntegerVector& startPhenxOfInterrest, int numOfThreads, bool removeSparseSequences, double sparsityValue, bool createTemporalBuckets, bool durationSparsity, double durationSparsityValue, bool removeSparseTemporalBuckets, int patIdLength, bool addDuration, bool durationInWeeks, bool durationInMonths);
+RcppExport SEXP _tSPMPlus_getSequencesWithCandidateEnd(SEXP df_dbMartSEXP, SEXP outputDirSEXP, SEXP outputFilePrefixSEXP, SEXP minDurationSEXP, SEXP bitShiftSEXP, SEXP lengthOfPhenxSEXP, SEXP numOfBucketsSEXP, SEXP lowerBucketThresholdsSEXP, SEXP startPhenxOfInterrestSEXP, SEXP numOfThreadsSEXP, SEXP removeSparseSequencesSEXP, SEXP sparsityValueSEXP, SEXP createTemporalBucketsSEXP, SEXP durationSparsitySEXP, SEXP durationSparsityValueSEXP, SEXP removeSparseTemporalBucketsSEXP, SEXP patIdLengthSEXP, SEXP addDurationSEXP, SEXP durationInWeeksSEXP, SEXP durationInMonthsSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< DataFrame& >::type df_dbMart(df_dbMartSEXP);
+    Rcpp::traits::input_parameter< std::string >::type outputDir(outputDirSEXP);
+    Rcpp::traits::input_parameter< std::string >::type outputFilePrefix(outputFilePrefixSEXP);
+    Rcpp::traits::input_parameter< unsigned long >::type minDuration(minDurationSEXP);
+    Rcpp::traits::input_parameter< unsigned int >::type bitShift(bitShiftSEXP);
+    Rcpp::traits::input_parameter< unsigned int >::type lengthOfPhenx(lengthOfPhenxSEXP);
+    Rcpp::traits::input_parameter< unsigned int >::type numOfBuckets(numOfBucketsSEXP);
+    Rcpp::traits::input_parameter< IntegerVector& >::type lowerBucketThresholds(lowerBucketThresholdsSEXP);
+    Rcpp::traits::input_parameter< IntegerVector& >::type startPhenxOfInterrest(startPhenxOfInterrestSEXP);
+    Rcpp::traits::input_parameter< int >::type numOfThreads(numOfThreadsSEXP);
+    Rcpp::traits::input_parameter< bool >::type removeSparseSequences(removeSparseSequencesSEXP);
+    Rcpp::traits::input_parameter< double >::type sparsityValue(sparsityValueSEXP);
+    Rcpp::traits::input_parameter< bool >::type createTemporalBuckets(createTemporalBucketsSEXP);
+    Rcpp::traits::input_parameter< bool >::type durationSparsity(durationSparsitySEXP);
+    Rcpp::traits::input_parameter< double >::type durationSparsityValue(durationSparsityValueSEXP);
+    Rcpp::traits::input_parameter< bool >::type removeSparseTemporalBuckets(removeSparseTemporalBucketsSEXP);
+    Rcpp::traits::input_parameter< int >::type patIdLength(patIdLengthSEXP);
+    Rcpp::traits::input_parameter< bool >::type addDuration(addDurationSEXP);
+    Rcpp::traits::input_parameter< bool >::type durationInWeeks(durationInWeeksSEXP);
+    Rcpp::traits::input_parameter< bool >::type durationInMonths(durationInMonthsSEXP);
+    rcpp_result_gen = Rcpp::wrap(getSequencesWithCandidateEnd(df_dbMart, outputDir, outputFilePrefix, minDuration, bitShift, lengthOfPhenx, numOfBuckets, lowerBucketThresholds, startPhenxOfInterrest, numOfThreads, removeSparseSequences, sparsityValue, createTemporalBuckets, durationSparsity, durationSparsityValue, removeSparseTemporalBuckets, patIdLength, addDuration, durationInWeeks, durationInMonths));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_tSPMPlus_createTransitiveSequences", (DL_FUNC) &_tSPMPlus_createTransitiveSequences, 5},
     {"_tSPMPlus_tSPMPlus", (DL_FUNC) &_tSPMPlus_tSPMPlus, 14},
     {"_tSPMPlus_extractNonSparseSequences", (DL_FUNC) &_tSPMPlus_extractNonSparseSequences, 8},
     {"_tSPMPlus_extractAllTransiviteSequences", (DL_FUNC) &_tSPMPlus_extractAllTransiviteSequences, 7},
+    {"_tSPMPlus_getSequencesWithCandidateEnd", (DL_FUNC) &_tSPMPlus_getSequencesWithCandidateEnd, 20},
     {NULL, NULL, 0}
 };
 
--- src/tSPMPlusInterface.cpp
+++ src/tSPMPlusInterface.cpp
@@ -191,11 +191,11 @@ DataFrame extractAllTransiviteSequences(DataFrame df_dbMart,
   
 }
 
-std::vector<temporalSequence> extractCandidatesForPhenx(std::vector<temporalSequence> &originalSequences,
+std::vector<temporalSequence> extractCandidatesSequences(std::vector<temporalSequence> &originalSequences,
                                     unsigned long minDuration, unsigned int bitShift,
                                     unsigned int lengthOfPhenx, unsigned int numOfBuckets,
                                     std::vector<unsigned int> lowerBucketThresholds,
-                                    std::vector<unsigned int> &startPhenxOfInterrest,
+                                    std::vector<unsigned int> startPhenxOfInterrest,
                                     int &numOfThreads){
   
   std::set<unsigned int> candidateEndPhenx;
@@ -205,13 +205,114 @@ std::vector<temporalSequence> extractCandidatesForPhenx(std::vector<temporalSequ
                                                         lengthOfPhenx,
                                                         startPhenxOfInterrest,
                                                         numOfThreads);
+  
+  Rcout<<"ExtractedCandidatePhenx: " << candidateEndPhenx.size() << std::endl;
+  
   std::vector<temporalSequence> sequencesOfInterest;
   sequencesOfInterest = extractSequencesWithEnd(originalSequences,
                                                 bitShift,
                                                 lengthOfPhenx,
                                                 candidateEndPhenx,
                                                 numOfThreads);
-  
+
   return sequencesOfInterest;
 }
 
+
+
+DataFrame transformToCandidateDataFrame(std::vector<temporalSequence> sequencesOfInterest,
+                                        std::vector<unsigned int> lowerBucketThresholds,
+                                        unsigned int lengthOfPhenx){
+  
+  std::vector<unsigned int> endPhenxVector;
+  endPhenxVector.reserve(sequencesOfInterest.size());
+  std::vector<unsigned int> durationBuckets;
+  durationBuckets.reserve(sequencesOfInterest.size());
+  std::vector<unsigned long> seqIDs;
+  seqIDs.reserve(sequencesOfInterest.size());
+  std::vector<int> patIDs;
+  patIDs.reserve(sequencesOfInterest.size());
+  std::vector<unsigned int> durations;
+  durations.reserve(sequencesOfInterest.size());
+  
+  for(temporalSequence seq : sequencesOfInterest){
+    int patId = seq.patientID;
+    long seqId = seq.seqID;
+    long duration = seq.duration;
+    unsigned int endPhenx = getEndPhenx(seq, lengthOfPhenx);
+    unsigned int durationBucket = getCandidateBucket(duration, lowerBucketThresholds);
+    patIDs.emplace_back(patId);
+    seqIDs.emplace_back(seqId);
+    durations.emplace_back(duration);
+    endPhenxVector.emplace_back(endPhenx);
+    durationBuckets.emplace_back(durationBucket);
+  }
+  
+  DataFrame  candidates = DataFrame::create(Named("patient_num") = patIDs, 
+                                            Named("sequence") = seqIDs,
+                                            Named("duration") = durations,
+                                            Named("endPhenx") = endPhenxVector,
+                                            Named("durationBucket") = durationBuckets);
+  return candidates;
+}
+
+
+
+// [[Rcpp::export]]
+DataFrame getSequencesWithCandidateEnd(DataFrame &df_dbMart,
+                                       std::string outputDir,
+                                       std::string outputFilePrefix,
+                                       unsigned long minDuration,
+                                       unsigned int bitShift,
+                                       unsigned int lengthOfPhenx,
+                                       unsigned int numOfBuckets,
+                                       IntegerVector &lowerBucketThresholds,
+                                       IntegerVector &startPhenxOfInterrest,
+                                       int numOfThreads = 1,
+                                       bool removeSparseSequences = true,
+                                       double sparsityValue = 0.05,
+                                       bool createTemporalBuckets = false,
+                                       bool durationSparsity = true,
+                                       double durationSparsityValue = 0,
+                                       bool removeSparseTemporalBuckets = false,
+                                       int patIdLength= 7,
+                                       bool addDuration = true,
+                                       bool durationInWeeks = false,
+                                       bool durationInMonths = false ){
+  
+  
+  if(numOfThreads <= 0){
+    numOfThreads = 1;
+  }
+  Rcout <<"Preparing data!\n";
+  Rcout.flush();
+  std::vector<dbMartEntry> dbMart = transformDataFrameToStruct(df_dbMart);
+  Rcout <<"Data prepared!\n";
+  Rcout.flush();
+  std::vector<temporalSequence> sequences =  sequenceWorkflow(dbMart,
+                                                              outputDir,
+                                                              outputFilePrefix,
+                                                              removeSparseSequences,
+                                                              sparsityValue,
+                                                              createTemporalBuckets,
+                                                              durationInWeeks,
+                                                              durationInMonths,
+                                                              durationSparsity,
+                                                              durationSparsityValue,
+                                                              removeSparseTemporalBuckets,
+                                                              patIdLength,
+                                                              numOfThreads);
+  
+  
+  std::vector<temporalSequence> sequencesOfInterest = extractCandidatesSequences(sequences,
+                                                                                 minDuration,
+                                                                                 bitShift,
+                                                                                 lengthOfPhenx,
+                                                                                 numOfBuckets,
+                                                                                 as<std::vector<unsigned int>>(lowerBucketThresholds),
+                                                                                 as<std::vector<unsigned int>>(startPhenxOfInterrest),
+                                                                                 numOfThreads);
+  Rcout<< sequencesOfInterest.size() << std::endl;
+    
+  return transformToCandidateDataFrame(sequencesOfInterest, as< std::vector<unsigned int> >(lowerBucketThresholds), lengthOfPhenx);
+}
